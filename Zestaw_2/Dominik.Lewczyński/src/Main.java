be548029a938b02ef01bbb4d0b5da162a60372cc
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-03-16 12:53:29 +0100

    Add Zestaw_2

---
+++ Zestaw_2/Dominik.Lewczyński/src/Main.java
@@ -0,0 +1,17 @@
+
+
+class StanOdkurzacza {
+    boolean[] lokalizacja;
+    boolean[] czy_brudne;
+}
+
+class Graf {
+    StanOdkurzacza[] graf;
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
